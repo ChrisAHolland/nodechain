c95160ea71612d41fd47538160de582d7b604061
Chris Holland <chris.holland@aot-technologies.com>
2019-08-27 15:08:00 -0700

    Able to create new blocks

--- src/block.js
+++ src/block.js
@@ -5,15 +5,15 @@
 const crypto = require('crypto-js')
 
 module.exports = class Block {
-    constructor(id, previousHash, timestamp, data) {
+    constructor(id, previousHash, data) {
         this.id = id
         this.previousHash = previousHash.toString()
         this.timestamp = Date.now()
-        this.data = datathis.hash = hash.toString
-        this.hash = _hash()
+        this.data = data
+        this.hash = this._hash()
     }
 
     _hash() {
-        return crypto.SHAW256(this.id + this.previousHash + this.timetamp + this.data).toString()
+        return crypto.SHA256(this.id + this.previousHash + this.timetamp + this.data).toString()
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 const express = require('express')
 const crypto = require('crypto-js')
 const bodyParser = require('body-parser')
+const Block = require('./block')
 
+// For testing
+myBlock = new Block(1, "abcdef", "Test")
+console.log(myBlock)
